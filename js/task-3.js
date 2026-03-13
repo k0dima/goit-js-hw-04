a1fc47031afa56935a2ee67c928f3363e37f15df
k0dima <214964089+k0dima@users.noreply.github.com>
2026-03-13 12:24:26 +0700

    complete hm 5

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,22 @@
-function filterArray(numbers, value) {
-  const res = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername: function (newName) {
+    this.username = newName;
+  },
+  updatePlayTime: function (hours) {
+    this.playTime += hours;
+  },
+  getInfo: function () {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-  for (const element of numbers) {
-    if (element > value) {
-      res.push(element);
-    }
-  }
+console.log("------ Profile ------");
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-  return res;
-}
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-console.log("------ Filter Array------");
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
